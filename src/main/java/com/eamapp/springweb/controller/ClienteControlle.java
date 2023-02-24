1d92500cbddf969776699ab11b758bff6bb2c4d3
heysebas <sebazgrisales@gmail.com>
2023-02-24 11:47:45 -0500

    Modolos vendedores

--- src/main/java/com/eamapp/springweb/controller/ClienteControlle.java
+++ src/main/java/com/eamapp/springweb/controller/ClienteControlle.java
@@ -1,16 +1,38 @@
 package com.eamapp.springweb.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.eamapp.springweb.models.entity.Cliente;
+import com.eamapp.springweb.models.entity.Vendedores;
 
 @Controller
 @RequestMapping("/cliente")
 public class ClienteControlle {
     
         @GetMapping({ "/", "", "/cliente" })
-        public String index(Model model) {
-            model.addAttribute("titulo", "Esta es la pagina del cliente");
+        public String indexClienteString(Model model) {
+            Cliente cliente = new Cliente();
+            model.addAttribute("titulo", "Perfilt");
+            model.addAttribute("cliente", cliente);
+            cliente.setNombre("sebastian");
+            cliente.setApellido("grisales");
+            cliente.setEmail("sebazgrisales@gmail.com");
+            model.addAttribute("cuerpo","perfil del cliente".concat(cliente.getNombre()));
             return "cliente/index";
+
+        }
+
+        @ModelAttribute("vendedores")
+        public List<Vendedores> poblarVededores(){
+            List<Vendedores> vendedores = Arrays.asList(new Vendedores("pepiyo","lopez", "pepito@prueba.com"),
+        new Vendedores("santi","hernandez", "santiago@prueba.com"));
+        return vendedores;
         }
     }
---
+++ src/main/java/com/eamapp/springweb/models/entity/Vendedores.java
@@ -0,0 +1,52 @@
+package com.eamapp.springweb.models.entity;
+
+public class Vendedores {
+
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    
+    public Vendedores() {
+    }
+
+
+    public Vendedores(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public String getApellido() {
+        return apellido;
+    }
+
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    
+}
